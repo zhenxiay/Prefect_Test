c307d19cc3b18ec0eb90de245090594a1c15a899
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-04-09 17:44:02 +0200

    Update Stock_Intelligence_Deployment.py

--- Deployments/Stock_Intelligence_Deployment.py
+++ Deployments/Stock_Intelligence_Deployment.py
@@ -1,1 +1,18 @@
+from prefect import flow
 
+# Source for the code to deploy (here, a GitHub repo)
+SOURCE_REPO="https://github.com/zhenxiay/Prefect_Test.git"
+
+if __name__ == "__main__":
+    flow.from_source(
+        source=SOURCE_REPO,
+        entrypoint="Test_data_ingestion_stock_intelligence.py:ingest_credit_risk",
+    ).deploy(
+        name="deployment-stock-intelligence",
+        parameters={
+            "db_name": "portfolio_analysis",
+            "stock_list": ['MSFT','ASML']
+        },
+        work_pool_name="data-ingestion-wp",
+        cron="0 6 * * 7",
+    )
